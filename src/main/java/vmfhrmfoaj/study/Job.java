3e35a6333d2f2686bc205037a7ac21981ce93a1e
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2021-06-29 15:25:13 +0900

    feat: TDD 연습 2. TranscodingService의 실패 시나리오 테스트를 통한 점진적 구현 추가
    실패상황에 대한 처리 방안
    Mock객체 생성시 다른 Mock객체에 이벤트 전달 되도록 함(사전정의)

---
+++ src/main/java/vmfhrmfoaj/study/Job.java
@@ -0,0 +1,45 @@
+package vmfhrmfoaj.study;
+
+public class Job {
+
+	public static enum State{
+		MEDIASOURCECOPYING,
+		COMPLETED
+	}
+	
+	private State state;
+	private Throwable occerredException;
+
+	public Object isLastState() {
+		return state;
+	}
+
+	public void changeState(State newState) {
+		this.state = newState;
+	}
+
+	public boolean isSuccess() {
+		return state == State.COMPLETED;
+	}
+
+	public boolean isWaiting() {
+		return state == null;
+	}
+
+	public boolean isFinished() {
+		return isSuccess() || isExceptionOccurred();
+	}
+
+	private boolean isExceptionOccurred() {
+		return (occerredException != null);
+	}
+
+	public Object getOccerredException() {
+		return occerredException;
+	};
+	
+	public void exceptionOccurred(RuntimeException e) {
+		this.occerredException = e;
+	}
+
+}
--- src/test/java/vmfhrmfoaj/study/TranscodingServiceImplTest.java
+++ src/test/java/vmfhrmfoaj/study/TranscodingServiceImplTest.java
@@ -1,6 +1,18 @@
 package vmfhrmfoaj.study;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -13,10 +25,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import vmfhrmfoaj.study.Job.State;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TranscodingServiceImplTest {
+	
+	private Long jobId = new Long(1);
 
 	@Mock
 	private MediaSourceCopier mediaSourceCopier;
@@ -33,16 +51,52 @@ public class TranscodingServiceImplTest {
 	@Mock
 	private JobResultNotifier jobResultNotifier;
 	
+	@Mock
+	private JobStateChanger jobStateChanger;
+
+	@Mock
+	private JobRepository jobRepository;
+	
+	@Mock
+	private JobExceptionHander jobExceptionHander;
+	
 	private TranscodingService transcodingService;
+	
+	private Job mockJob = new Job();
 	
 	@Before
 	public void setup() {
-		transcodingService = new TranscodingServiceImpl(mediaSourceCopier, transcoder, thumbnailExtractor, createdFileSender, jobResultNotifier);
+		transcodingService = new TranscodingServiceImpl(mediaSourceCopier, transcoder, thumbnailExtractor, createdFileSender, jobResultNotifier, jobStateChanger, jobExceptionHander);
+		
+		doAnswer(new Answer<Object>() {
+
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+				
+				State newState = (State)invocation.getArguments()[1];
+				mockJob.changeState(newState);
+				return null;
+			}
+			
+		}).when(jobStateChanger).changeJobState(anyLong(),  any(Job.State.class));;
+		
+		doAnswer(new Answer<Object>() {
+
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+				
+				RuntimeException ex = (RuntimeException)invocation.getArguments()[1];
+				mockJob.exceptionOccurred(ex);
+				return null;
+			}
+			
+		}).when(jobExceptionHander).notifyJobException(anyLong(), any(RuntimeException.class));
 	}
 
 	@Test
 	public void transcodeSuccessfully() {
-		Long jobId = new Long(1);
+		
+		when(jobRepository.findById(jobId)).thenReturn(mockJob);
 		
 		File mockMultimediaFile = mock(File.class);
 		when(mediaSourceCopier.copy(jobId)).thenReturn(mockMultimediaFile);
@@ -53,12 +107,47 @@ public class TranscodingServiceImplTest {
 		File mockThumnailFile = mock(File.class);
 		when(thumbnailExtractor.extractThumnail(mockMultimediaFile, jobId)).thenReturn(mockThumnailFile);
 		
+		Job job = jobRepository.findById(jobId);
+		assertTrue(job.isWaiting());
+		
 		transcodingService.transcode(jobId);
+		assertTrue(job.isFinished());
+		assertTrue(job.isSuccess());
+		assertEquals(Job.State.COMPLETED, job.isLastState());
+		assertNull(job.getOccerredException());
 		
 		verify(mediaSourceCopier, only()).copy(jobId);
 		verify(transcoder, only()).transcode(mockMultimediaFile, jobId);
 		verify(thumbnailExtractor, only()).extractThumnail(mockMultimediaFile, jobId);
 		verify(createdFileSender, only()).send(mockMultimediaFiles, mockThumnailFile, jobId);
 		verify(jobResultNotifier, only()).notifyJob(jobId);
+	}
+	
+	@Test
+	public void transcodeFailBecauseExceptionOccuredAtMediaSourceCopier() {
+		
+		when(jobRepository.findById(jobId)).thenReturn(mockJob);
+		
+		RuntimeException mockException = new RuntimeException();
+		when(mediaSourceCopier.copy(jobId)).thenThrow(mockException);
+		
+		try {
+			transcodingService.transcode(jobId);
+			fail("발생해야함");
+		} catch (Exception e) {
+			assertSame(mockException, e);
+		}
+		
+		Job job = jobRepository.findById(jobId);
+		assertTrue(job.isFinished());
+		assertFalse(job.isSuccess());
+		assertEquals(Job.State.MEDIASOURCECOPYING, job.isLastState());
+		assertNotNull(job.getOccerredException());
+		
+		verify(mediaSourceCopier, only()).copy(jobId);
+		verify(transcoder, never()).transcode(any(File.class), anyLong());
+		verify(thumbnailExtractor, never()).extractThumnail(any(File.class), anyLong());
+		verify(createdFileSender, never()).send(anyListOf(File.class), any(File.class), anyLong());
+		verify(jobResultNotifier, never()).notifyJob(anyLong());
 	}
 }
