f7255b172c4c1ea5425df7aa04530a7a72f9c9b7
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2021-06-29 18:10:27 +0900

    refactor: Job.transcode함수에서 상태관리를 sub 함수로 위임

--- src/main/java/vmfhrmfoaj/study/Job.java
+++ src/main/java/vmfhrmfoaj/study/Job.java
@@ -32,26 +32,23 @@ public class Job {
 
 		try {
 			// 미디어 원본으로부터 파일을 로컬에 복사한다.
-			changeJobState(Job.State.MEDIASOURCECOPYING);
 			File multimediaFile = copyMultimediaSourceToLocal();
 			
 			// 로컬에 복사된 파일을 변환처리한다.
-			changeJobState(Job.State.TRANSCODING);
 			List<File> multimediaFiles = transcode(multimediaFile, transcoder);
 			
 			// 로컬에 복사된 파일로부터 이미지를 추출한다.
-			changeJobState(Job.State.THUMBNAILEXTRACTING);
 			File thumbnail = extractThumbnail(multimediaFile, thumbnailExtractor);
 			
 			// 변환된 결과 파일과 썸네일 이미지를 목적지에 저장
-			changeJobState(Job.State.CREATEDFILESEND);
 			storeCreatedFilesToDestination(multimediaFiles, thumbnail);
 			
 			// 결과를 통보
-			changeJobState(Job.State.JOBRESULTNOTIFY);
 			notifyJob(jobResultNotifier);
 			
-			changeJobState(Job.State.COMPLETED);
+			// 완료처리
+			completed();
+			
 		} catch (RuntimeException e) {
 			exceptionOccurred(e);
 			throw e;
@@ -79,23 +76,32 @@ public class Job {
 	};
 
 	private File copyMultimediaSourceToLocal() {
+		changeJobState(Job.State.MEDIASOURCECOPYING);
 		return mediaSourceFile.getSourceFile();
 	}
 
 	private List<File> transcode(File multimediaFile, Transcoder transcoder) {
+		changeJobState(Job.State.TRANSCODING);
 		return transcoder.transcode(multimediaFile, id);
 	}
 
 	private File extractThumbnail(File multimediaFile, ThumbnailExtractor thumbnailExtractor) {
+		changeJobState(Job.State.THUMBNAILEXTRACTING);
 		return thumbnailExtractor.extractThumnail(multimediaFile, id);
 	}
 
 	private void storeCreatedFilesToDestination(List<File> multimediaFiles, File thumbnail) {
+		changeJobState(Job.State.CREATEDFILESEND);
 		destinationStorage.store(multimediaFiles, thumbnail, id);
 	}
 
 	private void notifyJob(JobResultNotifier jobResultNotifier) {
+		changeJobState(Job.State.JOBRESULTNOTIFY);
 		jobResultNotifier.notifyJob(id);
+	}
+
+	private void completed() {
+		changeJobState(Job.State.COMPLETED);
 	}
 
 	private void changeJobState(State newState) {
